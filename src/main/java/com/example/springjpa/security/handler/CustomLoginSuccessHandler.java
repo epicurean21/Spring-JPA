de252016cd46a04455b13b2bf63bbb346b043796
epicurean21 <jm_company@naver.com>
2021-09-16 17:26:55 +0900

    Spring Security - [Spring Security: handler, CustomLoginSuccessHandler]

---
+++ src/main/java/com/example/springjpa/security/handler/CustomLoginSuccessHandler.java
@@ -0,0 +1,20 @@
+package com.example.springjpa.security.handler;
+
+import com.example.springjpa.security.entity.User;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        final User user =
+    }
+}
